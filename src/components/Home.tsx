44a484f7110d5c22fb2cf1cdf65f7aa165ab874a
Ntezi <ngabomarius@gmail.com>
2024-07-06 18:20:05 +0200

    Deployment

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -7,7 +7,7 @@ const Home = () => {
             <div className="flex-grow flex flex-col justify-center items-center">
                 <div className="text-center">
                     <h1 className="text-4xl font-bold text-gray-800 mb-4">Welcome to the Ikizamini!</h1>
-                    <p className="mb-6 text-lg text-gray-600">This Ikizamini is here to help you practice and get ready for the theory exam for your driving test a.k.a Provisoir.</p>
+                    <p className="mb-6 text-lg text-gray-600">This Ikizamini is here to help you practice and get ready for the theory exam for your provisional driving license.</p>
                     <Link to="/quiz"
                           className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-lg shadow-lg transition duration-300 ease-in-out transform hover:-translate-y-1">
                         Tangira Ikizamini
